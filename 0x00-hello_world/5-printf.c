4c1d3850002e603cce59b6ed64f51da85f8c9525
codenvibes <terrymk1999@gmail.com>
2023-05-18 19:50:00 +0300

    Create File: 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints exactly with proper grammar, but the outcome
+ *	is a piece of art, , followed by a new line.
+ * Description: Using the printf function.
+ *	You are not allowed to use the function puts
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
