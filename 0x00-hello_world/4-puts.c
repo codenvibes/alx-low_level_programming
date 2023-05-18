a3940256858ea7a95d36c31ae21e206305502f59
codenvibes <terrymk1999@gmail.com>
2023-05-18 17:03:32 +0300

    Create File: 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints exactly "Programming is like building a
+ *	multilingual puzzle followed by a new line.
+ * Description: Use the function puts and not printf.
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
