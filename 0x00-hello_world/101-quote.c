b57c8b538f8af1b53369922454e96a87a3a37ec3
codenvibes <terrymk1999@gmail.com>
2023-05-18 20:59:48 +0300

    Create File: 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a C program that prints exactly and that piece of art is useful"
+ *	- Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ * Description: You are not allowed to use any functions listed in the NAME
+ *	section of the man (3) printf or man (3) puts.
+ *	Your program should return 1.
+ * Return: 0(success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
