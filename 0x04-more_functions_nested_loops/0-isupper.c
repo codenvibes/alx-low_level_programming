2f02985b670964842f8918dbd04992a4b1aa4a6d
codenvibes <terrymk1999@gmail.com>
2023-05-25 19:33:09 +0300

    Create File: 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point.
+ * Description: a function that checks for uppercase character.
+ * @c: character to be checked.
+ * Return: 1 if c is uppercase
+ *	0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
