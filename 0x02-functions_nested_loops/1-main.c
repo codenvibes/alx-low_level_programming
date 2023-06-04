31b6cb21545c030260fcd85b958d1db3a09d7703
codenvibes <terrymk1999@gmail.com>
2023-06-04 18:32:54 +0300

    test file

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lowercase
+ * Description: print alphabet in lowercase
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
