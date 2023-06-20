e0725b6e7b1fc083b9f7906f653ea0fa3ad23e34
codenvibes <terrymk1999@gmail.com>
2023-06-20 21:03:53 +0300

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * File: dog.h
+ * Auth: Terrence M K
+ * Desc: a header file that defines a new type struct dog.
+*/
+
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
+
