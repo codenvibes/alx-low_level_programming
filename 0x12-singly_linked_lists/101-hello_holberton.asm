40b6b9726a198040da903a083b5736257ae70445
codenvibes <terrymk1999@gmail.com>
2023-07-02 01:03:57 +0300

    Write a 64-bit program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,25 +1,25 @@
 section .data
-	hello db "Hello, Holberton", 0
-	format db "%s", 0
-	newline db 10, 0   ; ASCII code for newline character
+	hello db "Hello, Holberton", 0    ; Null-terminated string to be printed
+	format db "%s", 0		  ; Format string for printf
+	newline db 10, 0		  ; ASCII code for newline character
 
 section .text
 	extern printf
 
 global main
 main:
-	sub rsp, 8
-	mov rdi, format
-	mov rsi, hello
-	xor eax, eax
-	call printf
+	sub rsp, 8			   ; Allocate space on the stack
+	mov rdi, format			   ; Load format string address into rdi
+	mov rsi, hello			   ; Load string address into rsi
+	xor eax, eax			   ; Clear eax register
+	call printf			   ; Call printf to print the string
 
-	mov rdi, format
-	mov rsi, newline
-	xor eax, eax
-	call printf
+	mov rdi, format			   ; Load format string address into rdi
+	mov rsi, newline		   ; Load newline address into rsi
+	xor eax, eax			   ; Clear eax register
+	call printf			   ; Call printf to print the newline
 
-	add rsp, 8
-	xor eax, eax
-	ret
+	add rsp, 8			   ; Deallocate stack space
+	xor eax, eax			   ; Set return value to 0
+	ret				   ; Return from main
 
