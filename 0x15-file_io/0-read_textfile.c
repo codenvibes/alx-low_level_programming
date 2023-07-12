fd404f79d6c4b5283c188f62ad9ef67dda62825b
codenvibes <terrymk1999@gmail.com>
2023-07-12 16:08:26 +0300

    Create a function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,6 +4,7 @@
  */
 
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * read_textfile - Reads a text file and prints it to POSIX standard output.
@@ -14,40 +15,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, length, i, result;
+	ssize_t o, r, w;
 	char *buffer;
 
-	/*check if the parameter is NULL*/
 	if (filename == NULL)
 		return (0);
 
-	/*open the file in read only mode*/
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-
-	/*allocate a buffer of size letters*/
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	/*read and add a null terminator*/
-	read(fd, buffer, letters);
-	buffer[letters] = '\0';
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	for (i = 0; buffer[i] != '\0'; i++)
-		length += 1;
-
-	result = close(fd);
-	if (result != 0)
-		exit(-1);
-
-	/*write contents of buffer to STDOUT*/
-	result = write(STDOUT_FILENO, buffer, length);
-	if (result != length)
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
 		return (0);
+	}
 
 	free(buffer);
+	close(o);
 
-	return (length);
+	return (w);
 }
