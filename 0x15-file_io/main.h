368c640bf4312bf9f2208c1c87b0df5210345293
codenvibes <terrymk1999@gmail.com>
2023-07-12 16:13:33 +0300

    Create header file
    Header file containing prototypes of all functions used in the 0x15-file_io directory

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 /*
  * File: main.h
  * Auth: Terrence M.K
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x14-file_io directory.
  */
-
-#ifndef MAIN_H
-#define MAIN_H
 
 #include <sys/types.h>
 #include <sys/stat.h>
