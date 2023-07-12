add22b08f746fa38cd3e7115f753ffbb396d8544
codenvibes <terrymk1999@gmail.com>
2023-07-12 16:32:37 +0300

    Create header file
    Header file containing prototypes of all functions used in the 0x15-file_io directory

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,10 +10,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
